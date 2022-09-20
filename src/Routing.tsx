8c38102e617bd03ec408319ddcce1fbe8105a487
nujeyh <dza456@naver.com>
2022-09-20 18:53:17 +0900

    FEAT: 상세 페이지 price, chart 경로 추가

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Chart from "./components/Chart";
+import Price from "./components/Price";
 
 import Detail from "./pages/Detail";
 import Main from "./pages/Main";
@@ -8,7 +10,10 @@ const Routing = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path=":coinId" element={<Detail />} />
+        <Route path=":coinId" element={<Detail />}>
+          <Route path="price" element={<Price />} />
+          <Route path="chart" element={<Chart />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
