1fc0be8d1187134f1c81f992b2e0eb225480d1d2
nujeyh <dza456@naver.com>
2022-09-20 16:20:57 +0900

    FEAT: 코인 상세 페이지 연결

--- src/pages/Detail.tsx
+++ src/pages/Detail.tsx
@@ -1,9 +1,95 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+interface IInfoData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  is_new: boolean;
+  is_active: boolean;
+  type: string;
+  logo: string;
+  description: string;
+  message: string;
+  open_source: boolean;
+  started_at: string;
+  development_status: string;
+  hardware_wallet: boolean;
+  proof_type: string;
+  org_structure: string;
+  hash_algorithm: string;
+  links: object;
+  links_extended: object;
+  whitepaper: object;
+  first_data_at: string;
+  last_data_at: string;
+}
+
+interface IPriceData {
+  id: string;
+  name: string;
+  symbol: string;
+  rank: number;
+  circulating_supply: number;
+  total_supply: number;
+  max_supply: number;
+  beta_value: number;
+  first_data_at: string;
+  last_updated: string;
+  quotes: {
+    USD: {
+      price: number;
+      volume_24h: number;
+      volume_24h_change_24h: number;
+      market_cap: number;
+      market_cap_change_24h: number;
+      percent_change_15m: number;
+      percent_change_30m: number;
+      percent_change_1h: number;
+      percent_change_6h: number;
+      percent_change_12h: number;
+      percent_change_24h: number;
+      percent_change_7d: number;
+      percent_change_30d: number;
+      percent_change_1y: number;
+      ath_price: number;
+      ath_date: string;
+      percent_from_price_ath: number;
+    };
+  };
+}
 
 const Detail = () => {
   const { coinId } = useParams();
+  const [info, setInfo] = useState<IInfoData>();
+  const [price, setPrice] = useState<IPriceData>();
 
-  return <div>{coinId}</div>;
+  useEffect(() => {
+    (async () => {
+      const infoData = await (
+        await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
+      ).json();
+      const priceData = await (
+        await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
+      ).json();
+      setInfo(infoData);
+      setPrice(priceData);
+    })();
+  }, [coinId]);
+
+  return (
+    <>
+      <div>{info?.rank}</div>
+      <img src={info?.logo} />
+      <div>{info?.name}</div>
+      <div>{info?.description}</div>
+      <div>{info?.started_at}</div>
+      <div>{info?.symbol}</div>
+      <div>{price?.max_supply}</div>
+      <div>{price?.total_supply}</div>
+    </>
+  );
 };
 
 export default Detail;
