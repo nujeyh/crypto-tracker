f200fe3fe38fe0a05c0609978c8c41643e46fdfa
nujeyh <dza456@naver.com>
2022-09-21 20:10:50 +0900

    FEAT: 가격 히스토리 불러오기 추가

--- src/api.tsx
+++ src/api.tsx
@@ -1,17 +1,27 @@
 const BASE_URL = "https://api.coinpaprika.com/v1";
 
+// 모든 코인 목록 불러오기
 export const fetchCoins = () => {
   return fetch(`${BASE_URL}/coins`).then((response) => response.json());
 };
 
+// 코인 개별 정보 불러오기
 export const fetchCoinInfo = (coinId: string | undefined) => {
   return fetch(`${BASE_URL}/coins/${coinId}`).then((response) =>
     response.json()
   );
 };
 
+// 코인 개별 가격 불러오기
 export const fetchCoinPrice = (coinId: string | undefined) => {
   return fetch(`${BASE_URL}/tickers/${coinId}`).then((response) =>
     response.json()
   );
+};
+
+// 코인 개별 가격 히스토리 불러오기
+export const fetchCoinHistory = (coinId: string | undefined) => {
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+  ).then((response) => response.json());
 };
