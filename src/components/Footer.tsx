8726047c2da82ea7cafa97d85cc8f55db4e34879
nujeyh <dza456@naver.com>
2022-09-23 19:07:22 +0900

    CREATE: Footer 생성

---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+const Footer = () => {
+  return (
+    <Container>
+      <div>&copy; 2022 Hyejun</div>
+      <div>
+        <a href="https://github.com/nujeyh/crypto-tracker">
+          GO to GitHub &rarr;
+        </a>
+      </div>
+    </Container>
+  );
+};
+
+const Container = styled.footer`
+  text-align: center;
+  margin: auto;
+  padding: 10px;
+  max-width: ${(props) => props.theme.maxWidth};
+  color: ${(props) => props.theme.textSubColor};
+  div {
+    margin-bottom: 10px;
+  }
+  a {
+    color: ${(props) => props.theme.textMainColor};
+  }
+`;
+
+export default Footer;
