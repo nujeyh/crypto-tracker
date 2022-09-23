d9bf8a4204cfc011b28a6644fb7b8503ff1d8a16
nujeyh <dza456@naver.com>
2022-09-23 17:08:56 +0900

    EDIT: Footer 추가

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chart from "./components/Chart";
+import Header from "./components/Header";
 import Price from "./components/Price";
-
+import Footer from "./components/Footer";
 import Detail from "./pages/Detail";
 import Main from "./pages/Main";
 
 const Routing = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path=":coinId" element={<Detail />}>
@@ -15,6 +17,7 @@ const Routing = () => {
           <Route path="chart" element={<Chart />} />
         </Route>
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
