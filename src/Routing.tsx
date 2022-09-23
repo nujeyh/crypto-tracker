918109ca864fe7d1216dbb8b62263f7b5242c64f
nujeyh <dza456@naver.com>
2022-09-23 21:12:34 +0900

    EDIT: Float 추가

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Chart from "./components/Chart";
-import Header from "./components/Header";
 import Price from "./components/Price";
 import Footer from "./components/Footer";
 import Detail from "./pages/Detail";
 import Main from "./pages/Main";
+import Float from "./components/Float";
 
 const Routing = () => {
   return (
     <BrowserRouter>
-      <Header />
+      <Float />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path=":coinId" element={<Detail />}>
